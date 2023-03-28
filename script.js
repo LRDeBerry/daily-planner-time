2bf477f9cff4cb28b819df441940767968839099
Lee Deberry <ldeberry1992@gmail.com>
2023-03-27 21:39:14 -0700

    added issue with javascript clock/date

--- script.js
+++ script.js
@@ -103,3 +103,44 @@ $(function () {
   
   currentTime();
 });
+
+// below is prior date/time clock I tried to have but would not display correctly. 
+
+//function updateClock() {
+//  var now = new Date();
+ // var dayname = now.getDay(),
+//    mo = now.getMonth(),
+//    dnum = now.getDate(),
+//    yr = now.getFullYear(),
+//    hou = now.getHours(),
+//    min = now.getMinutes(),
+//    sec = now.getSeconds(),
+//    pe = "AM";
+
+//  if (hou == 0) {
+//    hou = 12;
+//  }
+//  if (hou > 12) {
+//    hou = hou - 12;
+//    pe = "PM";
+//  }
+
+//  Number.prototype.pad = function (digits) {
+//    for (var n = this.toString(); n.length < digits; n = 0 + n);
+//    return n;
+//  }
+
+//  var months = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//  var dates = ["Sunday", "Monday", "Tuesday", "Wendsday", "Thursday", "Friday", "Saturday"];
+//  var ids = ["dayname", "months", "dates", "year", "hours", "minutes", "seconds", "period"];
+//  var values = [dates[dayname], months[mo], dates[dnum], yr.pad(2), hou.pad(2), min.pad(2), sec.pad(2), pe];
+
+//  for (var i = 0; i < ids.length; i++)
+//    document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+//}
+// setInterval(initClock, 1000);
+
+//function initClock() {
+//  updateClock();
+//  window.setInterval(updateClock, 1);
+//}
